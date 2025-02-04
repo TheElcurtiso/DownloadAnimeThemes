f79ad8543c9181b45ce21451de93d1520c9ea6af
Elcurtiso <curtis_may@outlook.com>
2025-02-04 23:38:44 +0000

    Minor fixes

---
+++ README.md
@@ -0,0 +1,56 @@
+# Anime Openings and Endings Downloader
+
+`DISCLAIMER: This only works for MAL (MyAnimeList) at the moment! I might look into making it compatible for Anilist
+eventually!`
+
+Hi, if you're like me you like anime openings and endings and if you're really like me maybe you've spent a bit too much 
+time on [Anime Music Quiz](https://animemusicquiz.com/) and you need that little extra boost to start winning against your 
+friends. The thing is its hard to get all that music in one place and I agree! I mean I've seen nearly 200 shows now and 
+I can't keep track of everything that's going to come up on AMQ. Well this python script is the answer! Finally, you can 
+get all the music to jam to and train on in one convenient place!
+
+## Setup
+
+Now this script is super simple to set up. As I will lay out here:
+
+### Downloading and setting up the repository
+
+First, just clone or download the repository as a ZIP and put it wherever you like in your file explorer (as long as you
+can find it!). Open up your terminal and navigate to the folder you put the repo in. Make sure you have [Python](https://www.python.org/downloads/)
+installed - it shouldn't matter what version of Python 3 that you use but I'm using `3.12.8` if it makes you feel better
+and run the following line:
+
+`pip install -r requirements.txt`
+
+that should be all you need to not get any errors when running the python script.
+
+### Running the script
+
+The script takes two command line arguments: the username of the profile on MAL that you want to download the anime themes
+of and the client ID from MAL. The username should be clear enough to find - you can use your own or somebody else's it
+doesn't matter but the client ID is a bit more tricky as is laid out below:
+
+#### MyAnimeList Account
+
+You need a [MAL](https://myanimelist.net/) account (Sorry Anilist fans!!). Once you've created the account,
+you need to click on your user profile in the top right corner and click `Account Settings`. From there, click
+the furthest tab to the right labelled `API` then click the `Create ID` button. Its gonna bring you to a form where you
+need to write out the app name and type and all that but honestly? You can basically write anything in here (I literally
+wrote google.com as my redirect URL 😆) just fill it out and put the app type as `web` and the purpose of use as `hobbyist`.
+Once you've submitted that it will bring you back to the page before but now with a shiny `Edit` button below the `Create ID`
+button. Just click on the `Edit` button and you should now see a long string of numbers and letters next to Client ID.
+You just need to copy that (no need for the client secret).
+
+#### Last Steps
+
+You're in the home stretch now! You just need to go back to the terminal and run the script:
+
+`python downloadAnimeThemes.py <INSERT USERNAME> <INSERT CLIENT ID>`
+
+alternatively it could be this if the first doesn't work:
+
+`python3 downloadAnimeThemes.py <INSERT USERNAME> <INSERT CLIENT ID>`
+
+and you're done! It should now start to download all your favourite anime themes!
+
+The songs will be downloaded to a `songs` folder in the same place as the original python file.
---
+++ downloadAnimeThemes.py
@@ -0,0 +1,122 @@
+import argparse
+import os
+import shutil
+import time
+
+import requests
+import urllib.request
+from tqdm import tqdm
+
+
+def download_with_progress(url, download_path, original_file_name):
+    # Get the total file size
+    with urllib.request.urlopen(url) as response:
+        total_size = int(response.info().get("Content-Length", 0))
+
+    # Open the file and start downloading with progress tracking
+    with open(download_path, "wb") as file, tqdm(
+            desc=f"Downloading {original_file_name}",
+            total=total_size,
+            unit="B",
+            unit_scale=True,
+            unit_divisor=1024,
+    ) as bar:
+        def show_progress(block_num, block_size, total_size):
+            bar.update(block_size)  # Update the progress bar
+
+        urllib.request.urlretrieve(url, download_path, show_progress)
+
+    print(f"Downloaded {original_file_name}!")
+
+
+failed_titles = []
+def search_and_download_songs():
+    user_profile_url = f"https://api.myanimelist.net/v2/users/{args.username}/animelist?limit=1000&status=completed"
+
+    my_anime_list_response = requests.get(user_profile_url,
+                                          headers={'X-MAL-CLIENT-ID': f'{args.client_id}'})
+
+    delete_and_create_songs_directory()
+
+    if my_anime_list_response.status_code == 200:
+        my_anime_list_data = my_anime_list_response.json()
+        user_animes = my_anime_list_data['data']
+        # print("Response Data:", user_animes)
+        for anime in user_animes:
+            anime_url = f"https://api.myanimelist.net/v2/anime/{anime['node']['id']}?fields=id,title,alternative_titles,start_season"
+            my_anime_list_detailed_response = requests.get(anime_url,
+                                                           headers={
+                                                               'X-MAL-CLIENT-ID': f'{args.client_id}'})
+            if my_anime_list_detailed_response.status_code == 200:
+                my_anime_list_detailed_data = my_anime_list_detailed_response.json()
+                original_title = my_anime_list_detailed_data['title']
+                original_season = my_anime_list_detailed_data['start_season']['season']
+                original_year = my_anime_list_detailed_data['start_season']['year']
+                # print(my_anime_list_detailed_data)
+
+                anime_theme_url = f"https://api.animethemes.moe/search?page[limit]=1&fields[search]=anime&q={original_title}&include[anime]=animethemes.animethemeentries.videos"
+                anime_theme_search_response = requests.get(anime_theme_url)
+                anime_theme_search_data = anime_theme_search_response.json()
+                anime_themes_first_result = anime_theme_search_data['search']['anime'][0]
+                # print(anime_themes_first_result)
+
+                if original_season.lower() == anime_themes_first_result['season'].lower() and original_year == \
+                        anime_themes_first_result['year']:
+                    # print(anime_theme_search_data)
+                    print(f"Found {anime_theme_search_data['search']['anime'][0]['name']}!")
+                    for anime_theme in anime_theme_search_data['search']['anime'][0]['animethemes']:
+                        anime_theme_link = anime_theme['animethemeentries'][0]['videos'][0]['link']
+                        file_name = anime_theme_link.split("/")[-1]
+
+                        download_with_progress(anime_theme_link, "songs/" + file_name, file_name)
+                else:
+                    print("Initial search didn't work.")
+                    print("Checking alternate title...")
+                    anime_theme_url = f"https://api.animethemes.moe/search?page[limit]=1&fields[search]=anime&q={my_anime_list_detailed_data['alternative_titles']['en']}&include[anime]=animethemes.animethemeentries.videos"
+                    anime_theme_search_response = requests.get(anime_theme_url)
+                    anime_theme_search_data = anime_theme_search_response.json()
+                    # print(anime_theme_search_data)
+                    if "errors" in anime_theme_search_data:
+                        print("Something went wrong with the search.")
+                        failed_titles.append(original_title)
+                        continue
+                    anime_themes_first_result = anime_theme_search_data['search']['anime'][0]
+                    if original_season.lower() == anime_themes_first_result['season'].lower() and original_year == \
+                            anime_themes_first_result['year']:
+                        # print(anime_theme_search_data)
+                        print(f"Found {anime_theme_search_data['search']['anime'][0]['name']}!")
+                        for anime_theme in anime_theme_search_data['search']['anime'][0]['animethemes']:
+                            anime_theme_link = anime_theme['animethemeentries'][0]['videos'][0]['link']
+                            file_name = anime_theme_link.split("/")[-1]
+
+                            download_with_progress(anime_theme_link, "songs/" + file_name, file_name)
+                    else:
+                        print(f"Couldn't find {original_title}!")
+                        failed_titles.append(original_title)
+                        # print(original_title + " != " + anime_theme_search_data['search']['anime'][0]['name'])
+            else:
+                print("Failed to retrieve data:", my_anime_list_detailed_response.status_code)
+            time.sleep(1)
+    else:
+        print("Failed to retrieve data:", my_anime_list_response.status_code)
+
+    print("Sorry these titles couldn't be downloaded (maybe try and find them manually?): ")
+    for failed_title in failed_titles:
+        print(failed_title)
+
+
+def delete_and_create_songs_directory():
+    dir_path = "songs"
+    if os.path.exists(dir_path):
+        shutil.rmtree(dir_path)
+    os.makedirs(dir_path)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Download all the anime themes off a profile from MAL!")
+    parser.add_argument("username", help="The username of the profile you want to download songs from.")
+    parser.add_argument("client_id", help="The client ID from your MAL (MyAnimeList).")
+
+    args = parser.parse_args()
+
+    search_and_download_songs()
